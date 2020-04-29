a6a66683df1eaaf12be0b3b60e4736e47831874e
zamirmf <zamirmf@gmail.com>
2020-04-28 22:27:45 -0600

    Add README and CPP file.

---
+++ README.md
@@ -0,0 +1,49 @@
+# Running OpenGL using C/C++ on MacOS
+
+There is a good number of tutorials and code samples for MacOS out there. Those are usually based on IDE's like VSCode and XCode. More recently, there is a shift (probably a natural one) towards the trio Metal, Swift and XCode. OpenGL is already complex enough, so I thought that sharing the very bare bones of a sample code without many attachments would be useful for those either new to OpenGL or coming from a different platform.
+
+## Development environment
+
+I have been expanding and testing this code with the following setup:
+ - MacOS Catalina 10.15.3
+ - Glfw3: 3.3.2
+ - Glew: 2.1.0_1
+ - OpenGL: 4.1
+ - Apple clang version: 11.0.0
+
+With that said, this code should be supported with OpenGL greater than 3.3 (including). Just make sure that `GLFW_CONTEXT_VERSION_MAJOR`, `GLFW_CONTEXT_VERSION_MINOR` and shaders are updated accordingly.
+
+This is a comprehensive list of OpenGL versions across different Apple products: https://support.apple.com/en-ca/HT202823
+
+### Setting up the environment
+
+The easiest way to install Glfw and Glew is through Homebrew. In case you don't have it, go to https://brew.sh/ and you will get it done with a single command line.
+
+After that, glfw3 and glew can be installed with the following:
+```
+brew update
+brew install glfw3 glew
+```
+
+### Building the sample code
+
+The following command is enough to generate a binary from the source code file:
+```
+clang -o main main.cpp -L/usr/local/lib -lglfw -framework OpenGL
+```
+
+**Note** that different from GNU/Linux distros, MacOS has OpenGL added as a framework.
+
+
+### Running the solution
+
+Running `main` should provide a result similar to the following:
+
+![Screenshot of main](screenshot_of_main.png)
+
+## Recommended reading
+Apart from the official documentation ([OpenGL.org](https://www.opengl.org/) and [Kronos](https://www.khronos.org/registry/OpenGL/)), I suggest the following:
+
+ - https://learnopengl.com: As an old timer I was very surprised with the quality of this tutorial.
+ - https://www.glfw.org/docs/latest/: It might be prudent to get somehow familiar with the basics of GLFW.
+
---
+++ main.cpp
@@ -0,0 +1,167 @@
+#include <stdio.h>
+
+#define GL_SILENCE_DEPRECATION
+
+// Without this gl.h gets included instead of gl3.h
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
+
+// For includes related to OpenGL, make sure their are included after glfw3.h
+#include <OpenGL/gl3.h>
+
+void errorCallback(int error, const char* description)
+{
+    fputs(description, stderr);
+}
+
+void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+}
+
+void frameBufferResizeCallback(GLFWwindow* window, int width, int height){
+   glViewport(0, 0, width, height);
+}
+
+int main(void)
+{
+    GLFWwindow* window;
+
+    // Set callback for errors
+    glfwSetErrorCallback(errorCallback);
+
+    // Initialize the library
+    if (!glfwInit())
+        return -1;
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+
+    // Without these two hints, nothing above OpenGL version 2.1 is supported
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    // Create a windowed mode window and its OpenGL context
+    window = glfwCreateWindow(640, 480, "Running OpenGL on Mac", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    // Set callback for window
+    glfwSetKeyCallback(window, keyCallback);
+
+    // Set callback fro framebuffer
+    glfwSetFramebufferSizeCallback(window, frameBufferResizeCallback);
+
+    // Make the window's context current
+    glfwMakeContextCurrent(window);
+
+    // Used to avoid screen tearing
+    glfwSwapInterval(1);
+
+    //OpenGL initializations start from here
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+
+    /* Vertex array object*/
+    unsigned int VAO;
+    glGenVertexArrays(1, &VAO); 
+    glBindVertexArray(VAO);
+
+    // Vertex data and buffer
+    float vertices[] = {
+        -0.5f, -0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+        0.0f,  0.5f, 0.0f
+    };
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);  
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // Vertex shader
+    const char* vertexShaderSource = "#version 410 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\0";
+    unsigned int vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+    int  success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if(!success)
+    {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        fputs(infoLog, stderr);
+    }
+
+    // Fragment shader
+    const char* fragmentShaderSource = "#version 410 core\n"
+    "out vec4 FragColor;\n"
+    "void main()\n"
+    "{\n"
+    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "}\0";
+    unsigned int fragmentShader;
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if(!success)
+    {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        fputs(infoLog, stderr);
+    }
+
+    // Shader program
+    unsigned int shaderProgram;
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if(!success) {
+      glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+      fputs(infoLog, stderr);
+    }
+    glUseProgram(shaderProgram);
+
+    // Binding the buffers
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);  
+
+    //OpenGL initializations end here
+
+    // Loop until the user closes the window
+    while (!glfwWindowShouldClose(window))
+    {
+        // Resize the viewport
+        int width, height;
+        glfwGetFramebufferSize(window, &width, &height);
+        glViewport(0, 0, width, height);
+
+        // OpenGL Rendering related code
+        glClear(GL_COLOR_BUFFER_BIT);
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        // Swap front and back buffers
+        glfwSwapBuffers(window);
+
+        // Poll for and process events
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return 0;
+}
